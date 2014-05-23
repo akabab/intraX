c427d5becf7d5b25ae1d93f6682f2cdc6a0e234c
Alexandre DJIVAS <adjivas@e3r10p14.42.fr>
2014-05-23 21:24:18 +0200

    auth

--- routes/auths.js
+++ routes/auths.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET auths page. */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.render('auths', { title: 'authentification' });
+});
+
+router.post('/login', function (req, res) {
+  if (!req.body.pwd || !req.body.login)
+    res.send("empty variable");
+  else {
+    console.log(req.body.pwd, req.body.login);
+  }
 });
 
 module.exports = router;
