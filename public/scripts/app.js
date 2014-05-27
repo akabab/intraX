fdaab7c8f507673a59e8481020150f1c739cfe9a
Yoann CRIBIER <ycribier@e3r9p9.42.fr>
2014-05-27 21:20:02 +0200

    [Auth] before merge

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -30,26 +30,32 @@ app.config(function ($routeProvider, $locationProvider) {
   $locationProvider.html5Mode(true);
 });
 
+
 app.controller('AuthController', ['$scope', '$http', function ($scope, $http) {
 
   $scope.getForm = function () {
     console.log($scope.AuthForm);
   };
 
+  var rgxLogin = /^[A-Za-z0-9 ]{6,20}$/;
+  var rgxPassword = /^[A-Za-z0-9!@#$%^&*()_]{6,32}$/;
+
   $scope.errorMessage = '';
 
   $scope.signin = function() {
     var authForm = $scope.AuthForm;
-    if (authForm.login.$error.minlength)
-      $scope.errorMessage = "Invalid login";
+    $scope.errorMessage = '';
 
-    console.log($scope.login, $scope.password);
+    if (!rgxLogin.test($scope.login) || !rgxPassword.test($scope.password)) {
+      $scope.errorMessage = "Invalid data";
+      return ;
+    }
 
     $http.post('/auths', {
       'login': $scope.login,
       'password': $scope.password
     }).success(function (data, status) {
-      console.log(status + ': ' + data);
+      $scope.errorMessage = status + ": " + data;
     });
   };
 
