7000cfbaf8e592c4b5efe39798b6c2ba6c71abe4
Yoann CRIBIER <ycribier@e3r9p9.42.fr>
2014-05-29 15:54:14 +0200

    ~~

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.json({data:"hello"});
+router.get('/', function (req, res) {
+  res.render('template/user', user);
+  res.end();
 });
+
+router.post('/:name', function (req, res) {
+  
+});
+
+var user = {
+  name: "John",
+  lastname: "Doe",
+  uid: "41244",
+  city: "Paris",
+  age: 12
+};
 
 module.exports = router;
