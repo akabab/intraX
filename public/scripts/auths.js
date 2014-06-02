f30a98bd2f457024ff719e2f5c4dc9a6a970f689
Yoann CRIBIER <ycribier@e3r10p10.42.fr>
2014-06-02 15:49:54 +0200

    login ~

--- public/scripts/auths.js
+++ public/scripts/auths.js
@@ -3,21 +3,32 @@ angular.module('intraX').controller('AuthCtrl', function ($scope, $rootScope, $h
   var rgxLogin = /^[A-Za-z0-9 ]{3,20}$/;
   var rgxPassword = /^[A-Za-z0-9!@#$%^&*\(\)\{\}\[\]\?]{3,32}$/;
 
+  $scope.isRequesting = false;
   $scope.errorMessage = '';
+  $scope.message = 'Sign in';
 
   $scope.signin = function() {
+    // console.log($scope.login, $scope.password);
+
     $scope.errorMessage = '';
 
-    if (!rgxLogin.test($scope.login) || !rgxPassword.test($scope.password)) {
+    if (!$scope.login || !$scope.password || !rgxLogin.test($scope.login) || !rgxPassword.test($scope.password)) {
       $scope.errorMessage = "Invalid data";
       return ;
     }
 
+    $scope.isRequesting = true;
+    $scope.message = 'Signing in...';
+
     $http.post('/auths/signin', {
       'login': $scope.login,
       'password': $scope.password
-    }).success(function (data, status) {
-      if (!data.err) {
+    })
+    .success(function (data, status) {
+      $scope.isRequesting = false;
+
+        if (!data.err) {
+        $scope.errorMessage = '';
         SessionService.set("user", {
           firstName: data.user.firstName,
           lastName: data.user.lastName
@@ -26,6 +37,7 @@ angular.module('intraX').controller('AuthCtrl', function ($scope, $rootScope, $h
       }
       else {
         $scope.errorMessage = data.err;
+        $scope.message = 'Sign in';
       }
     });
   };
