e938fd215a9d0b18a936fe3103f7ab1d0bcc88e7
Yoann CRIBIER <ycribier@e3r10p10.42.fr>
2014-06-04 12:54:22 +0200

    load profile faster [fix router]

--- routes/user.js
+++ routes/user.js
@@ -12,7 +12,6 @@ var ldap = require('ldapjs');
 router.get('/:name', function (req, res) {
   var name = req.params.name;
   var account = req.session.account;
-
   var client = ldap.createClient({
     url: "ldaps://ldap.42.fr:636"
   });
@@ -29,7 +28,6 @@ router.get('/:name', function (req, res) {
         "first-name",
         "last-name",
         "mobile-phone",
-        "mail",
         "alias"
         ],
       filter:"!(close=non admis)",
@@ -40,14 +38,13 @@ router.get('/:name', function (req, res) {
         var user = {
           uid: entry.object['uid'],
           uidNumber: entry.object['uidNumber'],
-          picture: entry.object['picture'],
           birthDate: entry.object['birth-date'],
           firstName: entry.object['first-name'],
           lastName: entry.object['last-name'],
           mobilePhone: entry.object['mobile-phone'].replace(/ /g, ""),
-          mail: entry.object['mail'],
-          alias: entry.object['alias'],
-          picture: entry.raw['picture'].toString("base64")
+          mail: entry.object['alias'][2]
+          // alias: entry.object['alias']
+          // picture: entry.raw['picture'].toString("base64")
         };
         res.json( user );
       });
@@ -55,7 +52,7 @@ router.get('/:name', function (req, res) {
         res.json('referral: ' + referral.uris.join());
       });
       result.on('error', function (err) {
-        res.json('error: ' + err.message);
+        res.json({error: err.message});
       });
       result.on('end', function (result) {
         //console.log('status: ' + result.status);
