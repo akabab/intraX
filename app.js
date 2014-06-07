31c8242d7392adca3f4d5a0c07f4f7165eef3420
Clément Denis <cdenis@student.42.fr>
2014-06-07 18:33:48 +0200

    Added Inbox route

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var user  = require('./routes/user' );
 var auths = require('./routes/auths');
 var dltnt = require('./routes/dltnt');
 var forum = require('./routes/forum');
+var inbox = require('./routes/inbox');
 
 var app = express();
 
@@ -47,6 +48,7 @@ app.all("*", function(req, res, next) {
 app.use('/', index);
 app.use('/user', user);
 app.use('/forum', forum);
+app.use('/inbox', inbox);
 app.use('/dltnt', dltnt);
 
 
