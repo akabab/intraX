4fb7284275548c02ab9c17bf4df5e864eb5a9dff
Yoann CRIBIER <ycribier@e3r10p10.42.fr>
2014-06-10 20:05:48 +0200

    autologin

--- public/scripts/controllers/user.js
+++ public/scripts/controllers/user.js
@@ -3,14 +3,15 @@ angular.module('intraX')
 .controller('UserCtrl', function ($scope, $stateParams, $http, SessionService) {
   $scope.user = {};
   $scope.user.uid = $stateParams.uid;
-  
+  $scope.user.Found = false;
   $scope.isRequesting = true;
+  $scope.askingToken = false;
 
+  //Get User info
   $http.get('/user/' + $stateParams.uid).success(function (data) {
 
     if (!data.error) {
       $scope.user = data;
-      console.log(data);
       $scope.user.birthDate = data.birthDate.substring(6, 8) + '/' + data.birthDate.substring(4, 6) + '/' +data.birthDate.substring(0, 4);
       $scope.user.mail = data.uid + '@student.42.fr';
       $scope.user.found = true;
@@ -25,5 +26,45 @@ angular.module('intraX')
     $scope.isRequesting = false;
   });
 
+  $scope.askToken = function () {
+    $scope.askingToken = true;
+  }
+
+  $scope.getCurrentToken = function () {
+
+    console.log("yo");
+
+    $http.post('/auths/autologin', {'password': $scope.password})
+    .success(function (data) {
+      if (!data.err) {
+        $scope.askingToken = false;
+        $scope.token = data.token;
+      }
+      else
+        $scope.errorMessage = data.err;
+      })
+    .error(function (err) {});
+  };
+
+  $scope.getNewToken = function () {
+
+    $http.post('/auths/autologin/new', {'password': $scope.password})
+    .success(function (data) {
+      if (!data.err) {
+        $scope.token = data.token;
+      }
+      else
+        $scope.errorMessage = data.err;
+    })
+    .error(function (err) {});
+  };
+
+/*  (function() {
+    window.onmousemove = handleMouseMove;
+    function handleMouseMove(event) {
+        event = event || window.event; // IE-ism
+        console.log(event.clientX);// and event.clientY contain the mouse position
+    }
+})();*/
 
 });
--- routes/auths.js
+++ routes/auths.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var router = express.Router();
+var crypto = require('crypto');
 var bcrypt = require('bcrypt-nodejs');
 var easymongo = require('easymongo');
 var ldap = require('ldapjs');
@@ -14,6 +15,11 @@ var D_ERR_AUTHS_FINDNO    = "Account doesn't exist";
 var D_ERR_AUTHS_WRONGPWD  = "Invalid password";
 var D_ERR_AUTHS_TIMEFORCE = "Sorry, try to see this page more later.";
 
+
+//Render Authentication page
+router.get('/', function (req, res) {
+  res.render('auths', {title: 'Authentication'});
+});
 
 function connectToLdap(login, password, req, res) {
   var dn = 'uid=' + login + ',ou=2013,ou=people,dc=42,dc=fr';
@@ -37,6 +43,7 @@ function connectToLdap(login, password, req, res) {
         for (var i = 0; i < result.length; i++) {
           var account = result[i];
           if (bcrypt.compareSync(password, account['password'])) {
+            req.session.account['password'] = account['password']
             req.session.account['accessRights'] = account['accessRights'];
             req.session.account['_id'] = account['_id'];
             req.session['logged'] = true;
@@ -67,13 +74,15 @@ function connectToLdap(login, password, req, res) {
         accountsDB.find( {"login": login}, function (err, result) {
           if (!result.length) {
           //User not in database -> create new entry && log
+            var pwd = bcrypt.hashSync(password);
             accountsDB.save( {'login': login,
-                              'password': bcrypt.hashSync(password),
+                              'password': pwd,
                               'firstName': entry.object['first-name'],
                               'lastName': entry.object['last-name'],
                               'uid': entry.object['uid'],
                               'dateOfCreation': Date.now(),
                               'accessRights': 0}, function (error, result) {
+              req.session.account['password'] = pwd;
               req.session.account['accessRights'] = 0;
               req.session.account['_id'] = result['_id'];
               req.session['logged'] = true;
@@ -85,6 +94,7 @@ function connectToLdap(login, password, req, res) {
           for (var i = 0; i < result.length; i++) {
             var account = result[i];
             if (bcrypt.compareSync(password, account['password'])) {
+              req.session.account['password'] = account['password'];
               req.session.account['accessRights'] = account['accessRights'];
               req.session.account['_id'] = account['_id'];
               req.session['logged'] = true;
@@ -113,11 +123,7 @@ function connectToLdap(login, password, req, res) {
   });
 }
 
-
-router.get('/', function (req, res) {
-  res.render('auths', {title: 'Authentication'});
-});
-
+//Signin via authentication page
 router.post('/signin', function (req, res) {
   if (!req.body.password || !req.body.login) {
     return res.json( {err: D_ERR_AUTHS_EMPTY} );
@@ -136,14 +142,58 @@ router.post('/signin', function (req, res) {
   }
 });
 
-router.get('/autologin/:hash', function (req, res) {
-  accountsDB.find({'autologin': req.params.hash}, function (err, results) {
-    if (!results.length)
+//Get current autologin token
+router.post('/autologin', function (req, res) {
+  if (!req.body.password) {
+    return res.json( {err: D_ERR_AUTHS_EMPTY} );
+  }
+  var password = req.body.password;
+  if (bcrypt.compareSync(password, req.session.account['password'])) {
+    accountsDB.find({'password': req.session.account.password}, function (err, result) {
+      if (result.length) {
+        if (!result[0].autologin) {
+          res.json( {err: 'No current token'} );
+        }
+        else {
+          console.log(result[0].autologin);
+          res.json( {err: null, 'token': result[0].autologin} );
+        }
+      }
+    });
+  }
+  else
+    res.json( {err: D_ERR_AUTHS_WRONGPWD} );
+});
+
+//Generate new autologin token
+router.post('/autologin/new', function (req, res) {
+  if (!req.body.password) {
+    return res.json( {err: D_ERR_AUTHS_EMPTY} );
+  }
+  var password = req.body.password;
+  if (bcrypt.compareSync(password, req.session.account['password'])) {
+    crypto.randomBytes(24, function (ex, buf) {
+      var token = buf.toString('hex');
+      accountsDB.update({'password': req.session.account.password}, {$set: {'autologin': token}});
+      res.json( {err: null, 'token': token} );
+    });
+  }
+  else
+    res.json( {err: D_ERR_AUTHS_WRONGPWD} );
+});
+
+//Connect via autologin token
+router.get('/autologin/:token', function (req, res) {
+  var token = req.params.token;
+  accountsDB.find({'autologin': token}, function (err, result) {
+    if (!result.length)
       return res.redirect('/auths');
-    req.session.account = results[0];
+    req.session.account = result[0];
     req.session['logged'] = true;
     res.redirect('/');
   });
 });
+
+
 
 module.exports = router;
